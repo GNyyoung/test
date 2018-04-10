89c1754684a801c32b7a16287d71d30d4e4216c6
Park-YongJae <dydyong0710@gmail.com>
2018-04-10 19:27:56 +0900

    add

--- app/src/main/java/com/example/young/a201421045_week6/MainActivity.java
+++ app/src/main/java/com/example/young/a201421045_week6/MainActivity.java
@@ -7,10 +7,13 @@ public class MainActivity extends AppCompatActivity {
 
     //헤헤 주석 추가
     //주석이를 더 추가
+    //주석이를 삭제
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
